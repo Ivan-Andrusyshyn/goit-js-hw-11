c452731048b6086b89d263cd26f7e23836dabc6d
Ivan-Andrusyshyn <115649537+Ivan-Andrusyshyn@users.noreply.github.com>
2023-04-04 17:43:04 +0000

    Deploying to gh-pages from @ Ivan-Andrusyshyn/goit-js-hw-11@7bbcfd34db948594e3fc4bf02437d8064a60015e 🚀

---
+++ index.70cee341.js
@@ -0,0 +1,2 @@
+let e;document.querySelector('[type="checkbox"]').addEventListener("click",(t=>{!0===t.target.checked?e=setInterval((()=>{document.querySelector("body").style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3):clearInterval(e)}));
+//# sourceMappingURL=index.70cee341.js.map
---
+++ index.a386db66.js
@@ -0,0 +1,2 @@
+!function(){var t;document.querySelector('[type="checkbox"]').addEventListener("click",(function(e){!0===e.target.checked?t=setInterval((function(){document.querySelector("body").style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)))}),1e3):clearInterval(t)}))}();
+//# sourceMappingURL=index.a386db66.js.map
