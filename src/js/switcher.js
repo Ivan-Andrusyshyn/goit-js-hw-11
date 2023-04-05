20d69087d1300ea53b0fc14c43a20f4ba21946e2
Ivan Andrusyshyn <ivan.andrusyshyn@icloud.com>
2023-04-05 17:36:48 +0200

    s

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -1,26 +1,28 @@
 const nightInputCheck = document.querySelector('[type="checkbox"]');
 let intervalID;
+const btnForm = document.querySelector('.search-btn');
+const btnLoadMore = document.querySelector('[is-hidden]');
 const styleChangebtn = document.querySelectorAll('.btn');
 const inputChangeStyle = document.querySelector('input');
+const body = document.querySelector('body');
+
 nightInputCheck.addEventListener('click', e => {
   const checkbox = e.target.checked;
-
   if (checkbox === true) {
     intervalID = setInterval(() => {
-      const body = document.querySelector('body');
-      inputChangeStyle.style.borderColor = `${getRandomHexColor()}`;
-     inputChangeStyle.style.opacity = '0.5'
-      body.style.backgroundColor = `${getRandomHexColor()}`;
-
-      styleChangebtn.forEach(el => {
-        return (el.style.backgroundColor = `${getRandomHexColor()}`);
-      });
+      makeStyle();
     }, 1000);
   } else {
     stopChangeColorOnClick();
   }
 });
-
+function makeStyle() {
+  inputChangeStyle.style.borderColor = `${getRandomHexColor()}`;
+  inputChangeStyle.style.opacity = '0.5';
+  btnLoadMore.style.backgroundColor = `${getRandomHexColor()}`;
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  btnForm.style.backgroundColor = `${getRandomHexColor()}`;
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
